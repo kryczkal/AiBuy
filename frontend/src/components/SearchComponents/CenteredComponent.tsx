916d3383bc2e33039e165352a3517b56e85bcb23
Łukasz Kryczka <60490378+kryczkal@users.noreply.github.com>
2024-07-20 21:29:12 +0200

    Update CenteredComponent.tsx

--- frontend/src/components/SearchComponents/CenteredComponent.tsx
+++ frontend/src/components/SearchComponents/CenteredComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './CenteredComponent.scss';
 
 interface CenteredComponentProps {
-  children: React.ReactNode; // This allows any valid React node as a child
+  children: React.ReactNode;
 }
 
 const CenteredComponent: React.FC<CenteredComponentProps> = ({ children }) => {
