05131765ea97540f8615b5f0a2c1beb20b3b8c92
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2024-07-20 19:02:44 +0200

    Defined ValidateSearchPrompt endpoint

--- frontend/src/api/apiCalls.ts
+++ frontend/src/api/apiCalls.ts
@@ -1,28 +1,21 @@
+import axios from 'axios';
+import { log } from 'console';
+
 import { API_ENDPOINTS } from './apiConfig';
+
 
 interface ValidSearchPromptResponse {
   status: string;
 }
 export async function validateSearchPrompt(prompt : string) : Promise<boolean> {
   try {
-    const response = await fetch(API_ENDPOINTS.validateSearchResult, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ prompt }),
-    });
-    if(!response.ok) {
-      throw new Error('Failed to fetch results');
-    }
-    const data = await response.json() as ValidSearchPromptResponse;
-
-    return data.status === 'success';
+    const response = await axios.post<ValidSearchPromptResponse>(API_ENDPOINTS.validateSearchResult, { prompt });
+    return response.data.status === 'success';
   } catch (error) {
     if (error instanceof Error) {
-      console.error(error.message);
+      log(error.message);
     } else {
-      console.error('An unknown error occurred');
+      log('An unknown error occurred');
     }
   }
   return false;
