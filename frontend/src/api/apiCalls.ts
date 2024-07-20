5ca98c31f6bf4e7ff9b5e48b694d100fb07fcf6b
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2024-07-20 19:36:40 +0200

    Made Api endpoint for ItemRecommendations

--- frontend/src/api/apiCalls.ts
+++ frontend/src/api/apiCalls.ts
@@ -1,28 +1,42 @@
+import axios from 'axios';
+import { log } from 'console';
+
 import { API_ENDPOINTS } from './apiConfig';
 
-export async function validateSearchResult() : Promise<boolean> {
+import { ItemRecommendationBoxData } from 'src/models/ItemRecommendationBoxData';
+
+
+interface ValidSearchPromptResponse {
+  status: string;
+}
+export async function validateSearchPrompt(prompt : string) : Promise<boolean> {
   try {
-    const response = await fetch(API_ENDPOINTS.validateSearchResult, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if(!response.ok) {
-      throw new Error('Failed to fetch results');
-    }
-    const data = await response.json();
-
-    // TODO: Handle the fetched data
-    console.log('Fetched data:', data);
-
-    return true;
+    const response = await axios.post<ValidSearchPromptResponse>(API_ENDPOINTS.validateSearchResult, { prompt });
+    return response.data.status === 'success';
   } catch (error) {
     if (error instanceof Error) {
-      console.error(error.message);
+      log(error.message);
     } else {
-      console.error('An unknown error occurred');
+      log('An unknown error occurred');
     }
   }
   return false;
+};
+
+interface RecommendationResponse {
+  recommendations: ItemRecommendationBoxData[];
+}
+
+export async function getRecommendations(prompt : string) : Promise<ItemRecommendationBoxData[]> {
+  try {
+    const response = await axios.post<RecommendationResponse>(API_ENDPOINTS.getRecommendations, { prompt });
+    return response.data.recommendations;
+  } catch (error) {
+    if (error instanceof Error) {
+      log(error.message);
+    } else {
+      log('An unknown error occurred');
+    }
+  }
+  return [];
 };
--- frontend/src/api/apiConfig.ts
+++ frontend/src/api/apiConfig.ts
@@ -1,7 +1,8 @@
 const API_BASE_URL = '';
 
 export const API_ENDPOINTS = {
-  validateSearchResult: `${API_BASE_URL}/validate-search-result`,
+  validateSearchResult: `${API_BASE_URL}/`,
+  getRecommendations: `${API_BASE_URL}/`,
 
   // Example of a dynamic endpoint
   // getUser: (userId) => `${API_BASE_URL}/users/${userId}`,
