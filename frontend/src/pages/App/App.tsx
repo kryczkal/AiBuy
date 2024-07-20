d14a7d45d365d698d6e5bb0a7d945060c4716675
Jlisowskyy <Jlisowskyy@gmail.com>
2024-07-20 18:04:21 +0200

    Prepared based routes

--- frontend/src/pages/App/App.tsx
+++ frontend/src/pages/App/App.tsx
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+import NotFound from '../NotFound/NotFound';
+
 import SearchPage from 'src/pages/SearchPage/SearchPage';
 
 const App = () => {
   return (
     <div className='app'>
-      <SearchPage />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navigate to='/search' />} />
+          <Route path='/search' element={<SearchPage />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
