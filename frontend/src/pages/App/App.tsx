c59ae56d09ae1ebca29d9f1510936c3214468408
Łukasz Kryczka <60490378+kryczkal@users.noreply.github.com>
2024-07-21 01:32:37 +0200

    24 prepare visual components very likely simple task because the page should look similar to the search page (#44)
    * Added basic QuesstionComponent
    
    * Defined ValidateSearchPrompt endpoint
    
    * Defined ValidateSearchPrompt endpoint
    
    * Created Basic ItemRecommendationBox
    
    * Added ItemRecommendation Component
    
    * Delete frontend/src/components/ItemRecommendation/ItemRecommendation.tsx
    
    ---------
    
    Co-authored-by: Jakub Lisowski <115403674+Jlisowskyy@users.noreply.github.com>

--- frontend/src/pages/App/App.tsx
+++ frontend/src/pages/App/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import React from 'react';
 
 import NotFound from '../NotFound/NotFound';
+
 import QuestionComponentTest from '../TestPages/QuestionComponentTest';
 import ItemRecommendationBoxTest from '../TestPages/ItemRecommendationBoxTest';
 
@@ -17,7 +18,6 @@ const App = () => {
           <Route path='/' element={<Navigate to='/search' />} />
           <Route path='/search' element={<SearchPage />} />
           <Route path='*' element={<NotFound />} />
-
           // Test pages
 
           <Route path='/testQuestionComp' element={<QuestionComponentTest/>}/>
