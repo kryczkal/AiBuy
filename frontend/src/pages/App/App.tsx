d3e7aa557a6255c0ded51cdd6ad988eb804c153b
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2024-07-20 18:27:30 +0200

    Added basic QuesstionComponent

--- frontend/src/pages/App/App.tsx
+++ frontend/src/pages/App/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
 
 import NotFound from '../NotFound/NotFound';
 
 import SearchPage from 'src/pages/SearchPage/SearchPage';
-import React from 'react';
+import QuestionComponent from 'src/components/QuestionComponent/QuestionComponent';
 
-import SearchPage from 'src/pages/SearchPage/SearchPage';
 
 const App = () => {
   return (
@@ -15,6 +15,13 @@ const App = () => {
           <Route path='/' element={<Navigate to='/search' />} />
           <Route path='/search' element={<SearchPage />} />
           <Route path='*' element={<NotFound />} />
+
+          // Routes to test components
+          <Route path='/testQuestionComp' element={<QuestionComponent question="Test Question" onAnswerChange ={
+            (answer: string) => console
+          }
+                                                   />}
+          />
         </Routes>
       </Router>
     </div>
