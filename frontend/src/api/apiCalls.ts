0124cb37c49b30ee22189b2156203afebc8d3542
Jlisowskyy <Jlisowskyy@gmail.com>
2024-07-21 12:52:14 +0200

    Added process research api call

--- frontend/src/api/apiCalls.ts
+++ frontend/src/api/apiCalls.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { logger } from 'react-query/types/react/logger';
 
 import { API_ENDPOINTS } from './apiConfig';
+import { ProcessResearchFormat} from '../models/ProcessResearchFormat';
+import { ProcessResearchResults } from '../models/ProcessResearchResults';
 
 import { ProcessQueryResult } from 'src/models/ProcessQueryResult';
 import { ProcessQueryFormat } from 'src/models/ProcessQueryFormat';
@@ -15,6 +17,19 @@ export async function ProcessQueryApiCallThrowable(prompt : string, questions: s
 
   console.log('Sending process query request with payload: ', msg);
   const response = await axios.post<ProcessQueryResult>(API_ENDPOINTS.processQuery, { msg });
+  return response.data;
+}
+
+export async function ProcessResearchApiCallThrowable(name : string, description : string) : Promise<ProcessResearchResults> {
+  const msg : ProcessResearchFormat = {
+    name : name,
+    description : description,
+    questions : [],
+    answers : [],
+  };
+
+  console.log('Sending process research request with payload: ', msg);
+  const response = await axios.post<ProcessResearchResults>(API_ENDPOINTS.processResearch, { msg });
   return response.data;
 }
 
---
+++ frontend/src/models/ProcessResearchFormat.ts
@@ -0,0 +1,6 @@
+export interface ProcessResearchFormat {
+  name: string;
+  description: string;
+  questions: string[]; // currently unused
+  answers: string[]; // currently unused
+}
