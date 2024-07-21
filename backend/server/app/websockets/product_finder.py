554481e78d3ccb36cc648c013f816ec3483b0b62
Łukasz Kryczka <60490378+kryczkal@users.noreply.github.com>
2024-07-21 11:46:21 +0200

    Update product_finder.py

--- backend/server/app/websockets/product_finder.py
+++ backend/server/app/websockets/product_finder.py
@@ -1,27 +1,25 @@
-"""
+
 from fastapi import APIRouter, WebSocket
 from app.services.product_service import ProductService
-from .connection_manager import ConnectionManager
 
 router = APIRouter()
-manager = ConnectionManager()
 
 @router.websocket("/ws/product-finder")
 async def product_finder_websocket(websocket: WebSocket):
-    session_id = await manager.connect(websocket)
-    product_service = ProductService()
+    return
+    # session_id = await manager.connect(websocket)
+    # product_service = ProductService()
 
-    try:
-        while True:
-            data = await websocket.receive_json()
-            consumer_need = data.get('query')
-            if consumer_need:
-                product = await product_service.find_product(consumer_need)
-                await manager.send_personal_message({"session_id": session_id, "product": product.dict()}, session_id)
-            else:
-                await manager.send_personal_message({"error": "Invalid request format"}, session_id)
-    except Exception as e:
-        print(f"WebSocket error: {e}")
-    finally:
-        manager.disconnect(session_id)
-"""
+    # try:
+    #     while True:
+    #         data = await websocket.receive_json()
+    #         consumer_need = data.get('query')
+    #         if consumer_need:
+    #             product = await product_service.find_product(consumer_need)
+    #             await manager.send_personal_message({"session_id": session_id, "product": product.dict()}, session_id)
+    #         else:
+    #             await manager.send_personal_message({"error": "Invalid request format"}, session_id)
+    # except Exception as e:
+    #     print(f"WebSocket error: {e}")
+    # finally:
+    #     manager.disconnect(session_id)
